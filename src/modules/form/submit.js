76741170a12fe719f86c17df13348cf9e0abf609
WillMaac <anderrwil@gmail.com>
2026-02-14 11:47:03 -0300

    feat(schedule): exibe horários disponíveis para diferentes dias

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 
 const form = document.querySelector("form");
 const selectedDate = document.getElementById("date");
+const clientName = document.getElementById("client");
 
 //Data atual para formatar o input.
 const inutToday = dayjs(new Date()).format("YYYY-MM-DD");
@@ -12,5 +13,35 @@ selectedDate.min = inutToday;
 
 form.onsubmit = (event) => {
   event.preventDefault();
-  console.log("ENVIADO!");
+  try {
+    //Recuperando o nome do cliente
+    const name = clientName.value.trim();
+    if (!name) {
+      return alert("Informe o nome do clente!");
+    }
+
+    //Recupera o horário selecionado.
+    const hourSelected = document.querySelector(".hour-selected");
+    if (!hourSelected) {
+      return alert("Selecione a hora.");
+    }
+    //Recuperar somente a hora
+    const [hour] = hourSelected.innerText.split(":")
+  //Insere a hora na data
+  const when = dayjs(selectedDate.value).add(hour, "hour")
+  
+  //Gera um ID
+  const id = new Date().getTime()
+
+  console.log({
+    id,
+    name,
+    when
+  });
+  
+  
+    
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento");
+  }
 };
